b2d6c21a3d1396b7aaf9a8bf2b371e4a31429713
inoue <Inoue.Sosuke@technopro.com>
2023-06-12 15:53:04 +0900

    develop

--- src/main/resources/static/atm.js
+++ src/main/resources/static/atm.js
@@ -1,7 +1,7 @@
 /* ↓ 口座番号の取り扱いのためのヒント */
 let accountId;
 
-let amount;
+
 
 $(function(){
     $(".menuItem").on("click", function(){
@@ -9,72 +9,70 @@ $(function(){
     });
 
     $("#accountOpning").on("click", function(){
-        $("#accountOpningErea").addClass("none");
-        $("#accountMenu").removeClass("none");
+    	$.ajax({
+    		url: "/bankTrading/open",
+    		type: "POST",
+    		ContentType: "application/json",
+    	})
+    	.done(function(response) {
+    		$("#accountOpningErea").addClass("none");
+            $("#accountMenu").removeClass("none");
+    		accountId = response.id;
+    	})
+
     });
+
+
+    $("#checkBalance").on("click", function() {
+    	$.ajax({
+    		url: "/bankTrading/"+ accountId,
+    		type: "GET",
+    		ContentType: "application/json",
+    	})
+
+    	})
+
+
+    	$(".btn-primary1").on("click", function() {
+	    	const data = {
+	    		amount: $("#deposit2").val()
+	    	}
+
+    	$.ajax({
+    		url: "/bankTrading/deposit/" + accountId,
+    		type: "POST",
+    		ContentType: "application/json",
+    		data: JSON.stringify(data),
+    	})
+    	.done(function(response) {
+    		checkBalanceRewriting(response, amount)
+    	})
+})
+
+
+$(".btn-primary2").on("click", function() {
+	    	const data = {
+	    			amount: $("withdrawal2").val()
+	    	}
+	    	$.ajax({
+	    		url: "/bankTrading/withdraw/" + accountId,
+	    		type: "POST",
+	    		ContentType: "application/json",
+	    		data: JSON.stringify(data),
+	    	})
+	    	.done(function(response) {
+    		checkBalanceRewriting(response, amount)
+    	})
+    })
 
 
 });
 
 
-$(function() {
-	    $(".btn").on("click", function() {
-    	$.ajax({
-    		url: "/bankTrading/open",
-    		type: "POST",
-    	})
-    	.done(function(response) {
-    		accountId = response.accountId;
-    	})
-    })
-})
+function checkBalanceRewriting(amount) {
+    $("#checkBalanceAmount").text(amount);
+}
 
-
-$(function() {
-	    $("#checkBalance").on("click", function() {
-    	$.ajax({
-    		url: "/bankTrading/"+ accountId,
-    		type: "GET",
-    	})
-
-    	})
-    })
-})
-
-
-$(function() {
-	    $(".btn-primary1").on("click", function() {
-	    	amount = $("#deposit2").val()
-	    	console.log(amount)
-//    	$.ajax({
-//    		url: "/bankTrading/deposit/" + accountId,
-//    		type: "POST",
-//    		ContentType: "application/json",
-//    		data: amount,
-//    	})
-//    	.done(function(response) {
-//    		amount = response.requestAmount;
-//    })
-})
-})
-
-
-$(function() {
-	    $(".btn-primary2").on("click", function() {
-    	$.ajax({
-    		url: "/bankTrading/withdraw/" + accountId,
-    		type: "POST",
-    	})
-    })
-})
-
-
-
-$(document).ready(function() {
-	$.get("/" + accountId, function(response) {
-		$("#amountSpan").text(response.amount);
-	})
-})
 
 // メニューを選択した時の画面表示の切り替え
 // 引数はクリックされたDOM自身が入ってくるのを想定しておく
